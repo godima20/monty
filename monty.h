3d62205b962db2032e84c92b21dd2de35c573954
godima20 <godima20@gmail.com>
2022-09-07 19:49:30 +0000

    monty.c

--- monty.h
+++ monty.h
@@ -28,4 +28,63 @@ typedef struct stack_s
 	struct stack_s *next;
 } stack_t;
 /**
- * struct in
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: Function to operate the opcode
+ *
+ * Description - Opcode and its function for stack, queue, LIFO
+ * and FIFO for the project
+ */
+typedef struct instruction_s
+{
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+/**
+ * struct help - helper functions
+ * @token2: pointer to token 1
+ * @buffer: pointer to string returned from getline
+ * @fp: pointer to file
+ * @head: pointer to beginning of list
+ * @line_number: line number of file
+ * @n: number of bytes read
+ * @token1: pointer to token 2
+ * @queueflag: flag for queue function
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
+typedef struct help
+{
+	char *token2;
+	char *buffer;
+	FILE *fp;
+	stack_t *head;
+	size_t line_number;
+	size_t n;
+	char *token1;
+	int queueflag;
+} helpers;
+extern helpers helpy;
+
+void push(stack_t **head, unsigned int line_number);
+void pall(stack_t **head, unsigned int line_number);
+void pint(stack_t **head, unsigned int line_number);
+void free_dlistint(stack_t *head);
+int montyprocess(void);
+void free_everything(void);
+int _isnumber(void);
+void pop(stack_t **head, unsigned int line_number);
+void sw(stack_t **head, unsigned int line_number);
+void add(stack_t **head, unsigned int line_number);
+void nop(stack_t **head, unsigned int line_number);
+void sub(stack_t **head, unsigned int line_number);
+void divi(stack_t **head, unsigned int line_number);
+void mul(stack_t **head, unsigned int line_number);
+void mod(stack_t **head, unsigned int line_number);
+void pstr(stack_t **head, unsigned int line_number);
+void pchar(stack_t **stack, unsigned int line_number);
+int montycompare(void);
+void rotl(stack_t **head, unsigned int line_number);
+void rotr(stack_t **head, unsigned int line_number);
+int queuepush(stack_t **head, unsigned int line_number);
+#endif
